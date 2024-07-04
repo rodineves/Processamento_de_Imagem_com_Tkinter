c43ee84a2c5f3d60d98b7168a8588ebcbceb0e76
Rodrigo Neves Duarte <rodrigoneves@alu.uern.br>
2024-07-04 16:38:05 -0300

    adicionando codigo de teste

---
+++ teste.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Gerar uma matriz 10x10 de números aleatórios inteiros entre -255 e 255
+random_matrix = np.random.randint(-255, 256, (100, 100))
+
+# Criar o colormap com a faixa de valores de -255 a 255
+plt.imshow(random_matrix, cmap='seismic', vmin=-255, vmax=255)
+plt.colorbar()
+plt.title('Matriz de Números Aleatórios entre -255 e 255')
+plt.show()
